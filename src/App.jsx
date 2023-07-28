2d928aadc964312c599758b682753921d4442ddc
Saurabh Kumar Yadav <132044380+saurabhskills@users.noreply.github.com>
2023-07-28 01:07:54 +0000

    improved

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Saurabh Kumar Yadav",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  email: "sir.saurabh.cse@gmail.com",
+  gitHub: "@saurabhskills",
   instagram: "microsoft",
   linkedIn: "satyanadella",
   medium: "",
